31a4cb0c542f8fda1de801edfad42c4612516dc2
wyw <740484522@qq.com>
2023-08-15 18:04:30 +0800

    feat: 添加辅助提交提示

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -10,6 +10,7 @@ module.exports = {
       'always',
       [
         'feat', // 新功能 feature
+        'fix', // 修复
         'docs', // 文档注释
         'style', // 代码格式(不影响代码运行的变动)
         'refactor', // 重构(既不增加新功能，也不是修复bug)
